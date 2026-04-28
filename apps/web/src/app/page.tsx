67076a572a52c7acb41fadb51bff970b50c9e62a
Dominik Hryshaiev <domhryshaiev@gmail.com>
2026-04-28 15:10:25 +0200

    feat: add basic nextjs web app and typescript package config

---
+++ apps/web/src/app/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <main className="flex flex-1 w-full flex-col items-center gap-4 py-24 px-16 bg-gray-100 dark:bg-gray-800 sm:items-start text-center">
+      <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
+        Postroll
+      </h1>
+      <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
+        Coming soon to a browser near you.
+      </p>
+    </main>
+  );
+}
