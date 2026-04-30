465db33be2b44c5f11957b4de231671b2259361a
Dominik Hryshaiev <domhryshaiev@gmail.com>
2026-04-30 18:49:26 +0200

    chore(packages): add basic biome config for apps

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,20 +1,20 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next';
+import { Geist, Geist_Mono } from 'next/font/google';
+import './globals.css';
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
 });
 
 export const metadata: Metadata = {
-  title: "Postroll",
-  description: "Video files uploader, processor and analyzer",
+  title: 'Postroll',
+  description: 'Video files uploader, processor and analyzer',
 };
 
 export default function RootLayout({
