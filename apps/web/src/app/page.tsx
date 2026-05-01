f1a9c1dff6c2454c814bac91eed47b9fb1f468b1
Dominik Hryshaiev <domhryshaiev@gmail.com>
2026-05-01 18:03:12 +0200

    feat: add basic prisma database package (#5)
    * feat: add basic prisma database package
    
    * chore(turbo): add direct url to global env

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,4 +1,8 @@
-export default function Home() {
+import { prisma } from '@postroll/database';
+
+export default async function Home() {
+  const userCount = await prisma.user.count();
+
   return (
     <main className="flex flex-1 w-full flex-col items-center gap-4 py-24 px-16 bg-gray-100 dark:bg-gray-800 sm:items-start text-center">
       <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
@@ -7,6 +11,7 @@ export default function Home() {
       <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
         Coming soon to a browser near you.
       </p>
+      <p>There are {userCount} users in the database.</p>
     </main>
   );
 }
