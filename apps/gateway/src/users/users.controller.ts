c4bb06ce3e35efa26be1c9463544a38fca48b7bf
Dominik Hryshaiev <domhryshaiev@gmail.com>
2026-05-04 16:56:22 +0200

    fix: correctly set db url secret for fly.io pr deploy step

--- apps/gateway/src/users/users.controller.ts
+++ apps/gateway/src/users/users.controller.ts
@@ -1,12 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get } from "@nestjs/common";
 // biome-ignore lint/style/useImportType: needed for the decorator
-import { UsersService } from './users.service';
+import { UsersService } from "./users.service";
 
-@Controller('users')
+@Controller("users")
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
-  @Get('count')
+  @Get("/")
+  async getAll() {
+    return await this.usersService.getAll();
+  }
+
+  @Get("count")
   async getCount(): Promise<{ count: number }> {
     return { count: await this.usersService.getCount() };
   }
