ea3048d9ebcc580521c5b44eb7384d2231a76338
Dominik Hryshaiev <domhryshaiev@gmail.com>
2026-05-12 21:24:32 +0200

    feat(gateway): add pino logger

--- apps/gateway/src/app.module.ts
+++ apps/gateway/src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { LoggerModule } from 'nestjs-pino';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
@@ -11,6 +12,18 @@ import { UsersModule } from './users/users.module';
     ConfigModule.forRoot({
       isGlobal: true,
       validate: validateGatewayEnv,
+    }),
+    LoggerModule.forRoot({
+      pinoHttp: {
+        transport: {
+          target: 'pino-pretty',
+          options: {
+            colorize: true,
+            levelFirst: true,
+            translateTime: 'SYS:standard',
+          },
+        },
+      },
     }),
     DatabaseModule,
     UsersModule,
