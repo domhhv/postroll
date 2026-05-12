ae10e61b045350794ebedeef39db4325ea92f7c4
Dominik Hryshaiev <domhryshaiev@gmail.com>
2026-05-12 10:59:17 +0200

    build: make direct url optional in prisma config

--- packages/database/prisma.config.ts
+++ packages/database/prisma.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
     path: 'prisma/migrations',
   },
   datasource: {
-    url: env('DIRECT_URL'),
+    ...(process.env['DIRECT_URL'] && { url: env('DIRECT_URL') }),
     ...(process.env['SHADOW_DATABASE_URL'] && {
       shadowDatabaseUrl: process.env['SHADOW_DATABASE_URL'],
     }),
