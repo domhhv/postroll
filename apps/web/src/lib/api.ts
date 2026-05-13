0cf6343e631a778be09397a49d5a4801f4fa0db3
Dominik Hryshaiev <domhryshaiev@gmail.com>
2026-05-13 22:41:11 +0200

    fix(web): add no-store cache to users count endpoint call (#27)

--- apps/web/src/lib/api.ts
+++ apps/web/src/lib/api.ts
@@ -11,7 +11,7 @@ export async function getUsers(): Promise<User[]> {
 export async function getUserCount(): Promise<number> {
   const { GATEWAY_URL } = getServerEnv();
   console.log('Fetching user count from gateway, GATEWAY_URL: ', GATEWAY_URL);
-  const res = await fetch(`${GATEWAY_URL}/users/count`);
+  const res = await fetch(`${GATEWAY_URL}/users/count`, { cache: 'no-store' });
   const { count } = (await res.json()) as { count: number };
   return count;
 }
